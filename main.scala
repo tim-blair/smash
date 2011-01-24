cc97f1b1cfd4eb451cfa22533097229a39b80aa5
Tim Blair <trblair@gmail.com>
2011-01-23 23:00:31 -0500

    Catch exceptions, print msg, and keep going

--- main.scala
+++ main.scala
@@ -9,15 +9,18 @@ object ParseLine {
 			print("> ")
 			val line = reader.read
 			if(line != "exit") {
-				val (cmd, args) = parser.parse(line)
-				if(!builtinMgr.handle(cmd, args))
-					exec.execute(cmd, args)
+				try {
+					val (cmd, args) = parser.parse(line)
+					if(!builtinMgr.handle(cmd, args))
+						exec.execute(cmd, args)
+				} catch {
+					case e: Exception => println(e.getMessage)
+				}
 				process()
 			}
 		}
 
 		process()
-		//TODO: catch the exceptions thrown by the parser/etc
 		//TODO: have a custom exception type
 		//TODO: this doesn't work for things that run in the terminal (vim)
 	}
