079a8a17fcaa595b46137d1893c07508d0d07ab2
Tim Blair <trblair@gmail.com>
2011-01-29 00:23:49 -0500

    Instead of creating vals, use partially applied funcs

--- Suggester.scala
+++ Suggester.scala
@@ -4,13 +4,11 @@ object Suggester {
 	// maybe start with file based, and move to an actual learning based one
 	val favourites = List("vim", "scala", "ls", "cd", "cat")
 	val len = (x: String, cmd: String) => if(x.length == cmd.length) 1 else 0
-	val prefix = (x: String, cmd: String) => longestPrefix(x, cmd)
-	val subseq = (x: String, cmd: String) => longestSubseq(x, cmd)
-	val substr = (x: String, cmd: String) => longestSubstr(x, cmd)
 	val dist = (x: String, cmd: String) => -distance(x, cmd)
 	val favourite = (x: String, cmd: String) => if(favourites.contains(x)) 2 else 0
 
-	val filters = List( len, prefix, subseq, substr, favourite, dist) 
+	val filters = List( len, longestPrefix(_,_), longestSubseq(_,_), 
+		longestSubstr(_,_), favourite, dist) 
 
 	def score(cmd: String, x: String) = {
 		var total = 0
