bbc5666c12395ea1dab860719368f167aa50d315
Tim Blair <trblair@gmail.com>
2011-02-28 22:03:31 -0500

    Move out the parsing rules into a trait

--- LineParser.scala
+++ LineParser.scala
@@ -1,26 +1,6 @@
 import scala.util.parsing.combinator._
 
-//For now, no backslash, or any fanciness, really
-object LineParser extends RegexParsers {
-//Give me my whitespace!
-//thanks to http://oldfashionedsoftware.com/2008/08/16/easy-parsing-in-scala/
-//Prog in Scala says we can do: override val whiteSpace = "".r, too
-	override def skipWhitespace = false
-
-	// Not sure if '=' is valid for a cmd, but it is for an arg
-	def char: Parser[String] = """[-\w./+=]""".r
-	def ws: Parser[Unit] = """\s+""".r ^^ (x => Unit)
-	//TODO: single quotes
-	def string: Parser[List[String]] = "\"" ~> rep("""[^"]""".r) <~ "\""
-	def command: Parser[String] = rep(char) ^^ (_.mkString)
-	def argument: Parser[String] = (string | rep(char)) ^^ {
-		case x :: xs => (x :: xs).mkString 
-		case Nil => "" //empty list => empty string
-	}
-	def line: Parser[String ~ Option[List[String]]] = 
-		(command | failure("Could not parse command")) ~ 
-			opt(rep(ws ~> argument))
-
+object LineParser extends RegexParsers with LineParsing {
 	def parse(arg: String): (String, List[String]) = {
 		parseAll(line, arg.trim) match {
 			case Success((x ~ Some(list)), in) => (x, list)
--- TabCompleter.scala
+++ TabCompleter.scala
@@ -3,24 +3,7 @@ import scala.collection.mutable.Set
 import scala.util.parsing.combinator._
 import scala.collection.JavaConverters._
 
-object TabCompleter extends RegexParsers {
-	override def skipWhitespace = false
-
-	//TODO: share these rules with LineParser
-
-	// Not sure if '=' is valid for a cmd, but it is for an arg
-	def char: Parser[String] = """[-\w./+=]""".r
-	def ws: Parser[Unit] = """\s+""".r ^^ (x => Unit)
-	//TODO: single quotes
-	def string: Parser[List[String]] = "\"" ~> rep("""[^"]""".r) <~ "\""
-	def command: Parser[String] = rep(char) ^^ (_.mkString)
-	def argument: Parser[String] = (string | rep(char)) ^^ {
-		case x :: xs => (x :: xs).mkString 
-		case Nil => "" //empty list => empty string
-	}
-	def line: Parser[String ~ Option[List[String]]] = 
-		(command | failure("Could not parse command")) ~ 
-			opt(rep(ws ~> argument))
+object TabCompleter extends RegexParsers with LineParsing {
 
 	def parse(arg: String): (String, List[String]) = {
 		parseAll(line, arg.trim) match {
