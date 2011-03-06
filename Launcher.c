9924a5b0524e37aecaeab9715cff04e85999a48e
Tim Blair <trblair@gmail.com>
2011-03-06 12:52:29 -0500

    added a comment -- not important

--- Launcher.c
+++ Launcher.c
@@ -4,6 +4,7 @@
 #include "Launcher.h"
 
 JNIEXPORT void JNICALL Java_Launcher_runVim(JNIEnv *env, jobject obj) {
+	//file, char* argv[], char* envp[]
 	if( fork() == 0 )
 		execve("/usr/bin/vim", NULL, NULL);
 	else
